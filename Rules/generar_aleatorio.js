d98a21efbc7511478a3057e35b2fc1543e19520e
Santiago Castaño <santiago.castano@structum-co.com>
2023-04-28 07:55:06 +0000

    transacciones funcionando con aleatorio generico

--- Rules/generar_aleatorio.js
+++ Rules/generar_aleatorio.js
@@ -3,6 +3,6 @@
  * @param {IClientAPI} clientAPI
  */
 export default function generar_aleatorio(clientAPI) {
-    var aleatorio = Math.floor((Math.random() * (9999999999999- 100000000 + 1)) + 100000000);
+    var aleatorio = Math.floor((Math.random() * (99999999- 100000 + 1)) + 10000000);
     return aleatorio.toString();
 }
